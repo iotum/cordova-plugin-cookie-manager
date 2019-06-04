3ea88a578d594a737bf51f7563e00a118a501fcf
Mathieu Castets <mathieu.castets@4sh.fr>
2019-06-04 16:11:18 +0200

    Remove unused types.d.ts

--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface CookieManager {
-
-    /** Ensures all cookies currently accessible through the getCookie API are written to persistent storage. */
-    flush(): void;
-
-    /** Removes a cookie for the given domain */
-    remove(domain: string, cookieName: string): void;
-}
