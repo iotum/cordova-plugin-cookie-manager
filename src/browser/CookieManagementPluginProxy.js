cd3cb60a91e9abf18b840c25b580a38955ce3b31
Mathieu Castets <mathieu.castets@4sh.fr>
2019-06-04 16:42:08 +0200

    Fix browser platform

--- src/browser/CookieManagementPluginProxy.js
+++ src/browser/CookieManagementPluginProxy.js
@@ -1,10 +1,12 @@
 var CookieManagementPlugin = {
     flush: function (successCallback, errorCallback) {
         console.debug('flushing cookie (browser platform not supported)');
+        successCallback();
     },
     remove: function (successCallback, errorCallback, options) {
         console.log(options);
         // console.debug('removing cookie ' + cookieName + ' on domain ' + domain + ' (browser platform not supported)');
+        successCallback();
     }
 };
 
